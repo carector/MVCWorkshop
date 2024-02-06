b24370924d36bddefd5eca4782f0d54b181d2ad7
meelybug123 <emilysu36@gmail.com>
2024-02-05 17:25:02 -0800

    Trim whitespace in defs.js

--- src/defs.js
+++ src/defs.js
@@ -2,6 +2,4 @@ const BASE_URL = 'http://api.nessieisreal.com';
 const API_KEY = '';
 const ACCOUNT_ID = '';
 
-export { API_KEY, BASE_URL, ACCOUNT_ID }
-
-
+export { API_KEY, BASE_URL, ACCOUNT_ID };
